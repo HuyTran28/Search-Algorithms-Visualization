301c8ae9d81ec7e789368a35bce789ab40d2ebfa
PhatNguyen <23127449@student.hcmus.edu.vn>
2025-06-22 01:48:00 +0700

    dfs using stack

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -3,45 +3,37 @@ from algorithms.utils import reconstruct_path
 def dfs(problem, step_callback):
     start = problem.start
     goal = problem.goal
-    explored_count = [0]  # Use list to allow modification in nested scope
-    path_found = [False]  # Flag to stop recursion when goal is found
 
-    def df(node):
-        if path_found[0]:
-            return
+    stack = [start]
+    explored_count = 0
+
+    start.g = 0
+    start.parent = None
+
+    while stack:
+        node = stack.pop()
+        if node.explored:
+            continue
 
         node.explored = True
-        explored_count[0] += 1
+        explored_count += 1
         step_callback()
         yield
 
         if problem.is_goal(node):
-            path_found[0] = True
+            path, cost = reconstruct_path(goal)
+            for node in path:
+                node.in_path = True
+                step_callback()
+                yield
+            yield (explored_count, cost)
             return
 
-        for neighbor in problem.get_neighbors(node):
-            if not neighbor.explored and not path_found[0]:
+        neighbors = problem.get_neighbors(node)
+        for neighbor in reversed(neighbors):
+            if not neighbor.explored:
                 neighbor.parent = node
                 neighbor.g = node.g + neighbor.cost
-                yield from df(neighbor)
+                stack.append(neighbor)
 
-        # Backtrack (only clear highlight if not part of final path)
-        if not path_found[0]:
-            node.explored = False
-            step_callback()
-            yield
-
-    start.g = 0
-    yield from df(start)
-
-    if path_found[0]:
-        path, cost = reconstruct_path(goal)
-        for node in path:
-            node.in_path = True
-            step_callback()
-            yield
-        yield (explored_count[0], cost)
-        return
-
-    yield (None, explored_count[0], 0)
-    
+    yield (explored_count, 0)
